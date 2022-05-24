62dfd276ee28854c46f9ec26a176b5e9e970d087
sydbang <sydney.bang94@gmail.com>
2022-05-24 16:52:08 -0400

    fix feature view
    - feature view now reads from core data
    - uses fetch request with predicate instead of loading all recipes and checking for  if "featured" == true

--- Recipe List App/Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Recipe List App/Views/RecipeFeaturedView.swift
@@ -9,33 +9,36 @@ import SwiftUI
 
 struct RecipeFeaturedView: View {
     
-    @Environment(\.managedObjectContext) private var viewContext
+    //@Environment(\.managedObjectContext) private var viewContext
+    //@EnvironmentObject var model:RecipeModel
     
-    @EnvironmentObject var model:RecipeModel
     @State var isDetailViewShowing = false
     @State var tabSelectionIndex = 0
+    
+    // Fetch only if featured is true
+    @FetchRequest(sortDescriptors: [NSSortDescriptor(key: "name", ascending: true)], predicate: NSPredicate(format: "featured == true")) var recipes: FetchedResults<Recipe>
     
     var body: some View {
         
         VStack(alignment: .leading, spacing: 0) {
-        
+            
             Text("Featured Recipes")
                 .bold()
                 .padding(.leading)
                 .padding(.top, 40)
                 .font(Font.custom("Avenir Heavy", size: 24))
-                
+            
             
             GeometryReader { geo in
-            
-            TabView (selection: $tabSelectionIndex) {
                 
-                // Loop through each recipe
-                ForEach (0..<model.recipes.count) { index in
+                TabView (selection: $tabSelectionIndex) {
                     
-                    // Only show those that should be featured
-                    if model.recipes[index].featured == true {
-                    
+                    // Loop through each recipe
+                    ForEach (0..<recipes.count) { index in
+                        
+                        //                    // Only show those that should be featured
+                        //                    if recipes[index].featured == true {
+                        
                         // Recipe card button
                         Button(action: {
                             
@@ -48,17 +51,17 @@ struct RecipeFeaturedView: View {
                             ZStack {
                                 Rectangle()
                                     .foregroundColor(.white)
-                                    
+                                
                                 
                                 VStack(spacing: 0) {
                                     
-                                    let image = UIImage(data: model.recipes[index].image ?? Data()) ?? UIImage()
+                                    let image = UIImage(data: recipes[index].image ?? Data()) ?? UIImage()
                                     
                                     Image(uiImage: image)
                                         .resizable()
                                         .aspectRatio(contentMode: .fill)
                                         .clipped()
-                                    Text(model.recipes[index].name)
+                                    Text(recipes[index].name)
                                         .padding(5)
                                         .font(Font.custom("Avenir", size: 15))
                                 }
@@ -68,32 +71,32 @@ struct RecipeFeaturedView: View {
                         .tag(index)
                         .sheet(isPresented: $isDetailViewShowing) {
                             // Show the Recipe Detail View
-                            RecipeDetailView(recipe: model.recipes[index])
+                            RecipeDetailView(recipe: recipes[index])
                         }
                         .buttonStyle(PlainButtonStyle())
                         .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                         .cornerRadius(15)
                         .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
                         
+                        //                    }
                     }
+                    
                 }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
                 
             }
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
-            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
             
-        }
-        
             VStack (alignment: .leading, spacing: 10) {
                 
                 Text("Preparation Time:")
                     .font(Font.custom("Avenir Heavy", size: 16))
-                Text(model.recipes[tabSelectionIndex].prepTime)
+                Text(recipes[tabSelectionIndex].prepTime)
                     .font(Font.custom("Avenir", size: 15))
                 
                 Text("Highlights")
                     .font(Font.custom("Avenir Heavy", size: 16))
-                RecipeHighlights(highlights: model.recipes[tabSelectionIndex].highlights)
+                RecipeHighlights(highlights: recipes[tabSelectionIndex].highlights)
             }
             .padding([.leading, .bottom])
         }
@@ -105,7 +108,7 @@ struct RecipeFeaturedView: View {
     func setFeaturedIndex() {
         
         // Find the index of first recipe that is featured
-        let index = model.recipes.firstIndex { (recipe) -> Bool in
+        let index = recipes.firstIndex { (recipe) -> Bool in
             return recipe.featured
         }
         tabSelectionIndex = index ?? 0
