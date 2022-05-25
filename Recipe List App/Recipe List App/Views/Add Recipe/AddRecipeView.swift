4a74a3ff206c1f0daabb6476ea230fa07f1cef89
sydbang <sydney.bang94@gmail.com>
2022-05-25 14:41:57 -0400

    organize view files
    - create new add recipe view file

---
+++ Recipe List App/Recipe List App/Views/Add Recipe/AddRecipeView.swift
@@ -0,0 +1,20 @@
+//
+//  AddRecipeView.swift
+//  Recipe List App
+//
+//  Created by Sunghee Bang on 2022-05-25.
+//
+
+import SwiftUI
+
+struct AddRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddRecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddRecipeView()
+    }
+}
